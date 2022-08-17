e188c48f7167b31824a6b0c19a835f57f814e6f9
Vojtěch Létal <vojtech.letal@avast.com>
2022-08-17 11:50:28 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Fastformer-Keras',
+    version='1.0.0',
+    url='https://github.com/letalvoj/Fastformer-Keras',
+    packages=find_packages(),
+    install_requires=['tensorflow'],
+)
