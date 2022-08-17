978296649ef15d2408ae5378254029899a96e182
Vojtěch Létal <vojtech.letal@avast.com>
2022-08-17 11:13:35 +0200

    add imports to __init__.py

---
+++ fastformer/__init__.py
@@ -0,0 +1,5 @@
+from fastformer import Fastformer
+from fastformer import Time2Vector
+from fastformer import SingleAttention
+from fastformer import MultiAttention
+from fastformer import TransformerEncoder
